74a7500552c21dfcef705951281d0f4c38c173c9
Rohit Shrivastava <RohitSh@emids.com>
2020-02-04 20:17:51 +0530

    adding axios to call API

--- src/sagas/studySaga.js
+++ src/sagas/studySaga.js
@@ -1,10 +1,10 @@
-// import { call } from 'redux-saga/effects';
-// import { getStudies } from '../actions/studyActions';
+import { call } from 'redux-saga/effects'
+import { getStudies } from '../actions/studyActions'
 
 export function* getStudySaga() {
     // eslint-disable-next-line prettier/prettier
     yield console.log("saga called")
-    //   const studies = yield call(getStudies)
-    //   console.log("after saga set studies", studies)
+    const studies = yield call(getStudies)
+    console.log('after saga set studies', studies)
     // yield put({ type: "SET_STUDIES", studies: studies.articles });
 }
