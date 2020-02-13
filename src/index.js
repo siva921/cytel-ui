21c9884ba93c2f6b8d36b867601921019ab228a5
Rohit Shrivastava <RohitSh@emids.com>
2020-02-13 11:51:55 +0530

    fontawesome icons, json-server added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import ReactDOM from 'react-dom'
 import App from './app'
 import './styles.css'
 import './styles.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTrashAlt)
 
 var mountNode = document.getElementById('app')
 ReactDOM.render(<App name="Jane" />, mountNode)
